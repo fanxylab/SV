3cea650e33fc30a8d9574d2a8b1347b904632360
fanxylab <82557417+fanxylab@users.noreply.github.com>
2021-10-13 11:18:25 +0800

    Update bam_proc.py

--- bam_proc.py
+++ bam_proc.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python
 
 **********************************************************
-* @File    : bam_proc.py					 *				
+* @File    : bam_proc.py			         *				
 * @Author  : WANG Jun                                    *
 * @Date    : 2021/03                                     *
 * @E-mail  : oucwj@outlook.com                           *               
